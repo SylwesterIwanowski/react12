1c72e6fca0c0331a5bf7765300d2a3e2202b597f
SylwesterIwanowski <50060453+SylwesterIwanowski@users.noreply.github.com>
2021-12-26 12:47:49 +0100

    add components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+const App = () => {
+  return (
+    <div>
+      <Heading />
+      <List />
+      <List />
+    </div>
+  );
+};
+
+export default App;
